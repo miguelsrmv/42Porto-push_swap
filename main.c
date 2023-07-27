96fd2ceae4875729f6f6918910c19f9ae9ea6877
Miguel De Sa  Rodrigues Moura Vieira <mde-sa--@c1r15s3.42porto.com>
2023-07-27 15:52:21 +0100

    Day 1

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mde-sa-- <mde-sa--@student.42porto.com     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 19:13:55 by mde-sa--          #+#    #+#             */
-/*   Updated: 2023/07/27 14:49:43 by mde-sa--         ###   ########.fr       */
+/*   Updated: 2023/07/27 15:21:20 by mde-sa--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,13 +57,21 @@ int check_duplicates(t_list *starting_node)
 		current_node = starting_node->next;
 		while(current_node->value)
 		{
+			printf("Starting node has value %i and current node has value %i\n", starting_node->value, current_node->value);
+			fflush(stdout);
 			if (starting_node->value == current_node->value)
-				return (1);
-			current_node = current_node->next;
+				return (0);
+			if (current_node->next)
+				current_node = current_node->next;
+ 			else
+				break;
 		}
+		printf("\n\n");
 		starting_node = starting_node->next;
 	}
-	return (0);
+	printf("YAY!");
+	fflush(stdout);
+	return (1);
 }
 
 int	main(int argc, char **argv)
@@ -80,7 +88,9 @@ int	main(int argc, char **argv)
 		write(1, "Error\n", 6);
 		return (1);
 	}
-	print_list(starting_node);
+	/* print_list(starting_node); */
+
+	
 	// Sort values
 	// Delete the entirety of the linked list
 	return (0);
